c2eac87eccd3596b1c9ee3ce48cf54c71481d515
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2023-11-23 09:13:56 -0500

    Configuration that builds on RHEL9

--- dependencyInstall.sh
+++ dependencyInstall.sh
@@ -2,12 +2,20 @@
 
 # Make sure to enable the codeready builder RHEL repos first
 
+# The following packages are currently missing on RHEL9
+dnf -y install boost-devel cmake giflib-devel libXext-devel \
+    libXt-devel libXtst-devel libraw1394 libtirpc-devel \
+    libusb-devel libusbx-devel libxml2-devel motif-devel \
+    net-snmp-devel opencv-devel pcre-devel re2c readline-devel \
+    rpcgen xz-devel zeromq-devel
+
 # Needed for EPICS base + core modules
 dnf -y install re2c readline-devel
 dnf -y install libxml2-devel pcre-devel libtirpc-devel
 dnf -y install libusbx-devel libXext-devel libjpeg-devel perl-devel
 dnf -y install git wget tar make cmake gcc gcc-c++ pkgconfig
 dnf -y install libraw1394 boost-devel libusb-devel rpcgen
+dnf -y install qt5-qtbase-devel
 
 # Needed for SNMP EPICS module
 dnf -y install net-snmp-devel
@@ -27,3 +35,5 @@ dnf -y install opencv-devel
 # Needed for ffmpegServer
 dnf -y install xz-devel
 
+# This is needed on RHEL9 since the opencv2 install dir has apparently changed
+ln -s /usr/include/opencv4/opencv2 /usr/include/opencv2
